0cf852c793df94bea18380efc595584865ffc9d8
5Chanseok <ckstjr95@gmail.com>
2023-03-27 13:45:38 +0900

    feat: 블루프린트를 이용한 페이지 구성

--- advance/step2_app_factory/service/__init__.py
+++ advance/step2_app_factory/service/__init__.py
@@ -11,9 +11,24 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def home():
-        return 'home page 커스텀'
+    init_blueprint(app)
     
     return app
 
+def init_blueprint(app):
+    # app에 블루프린트 객체를 등록한다.
+
+    # 실습 ~~~/auth/ 접속 시 인증홈이라는 내용이 나오도록 auth 관련 블루프린트를 구성하시오
+
+    # 블루프린트로 정의된 개별 페이지 관련 내용 로드 -> 땡겨주기만 해도 실행되어 메모리에 올라간다.(연동된다.)
+    from .controllers import main_controller
+    from .controllers import auth_controller
+
+    # from service.controllers import bp_main
+    # 이 위치에서는 service를 생략하고 표현 가능
+    from .controllers import bp_main, bp_auth
+    
+    # 플라스크 객체 블루프린트 등록
+    app.register_blueprint(bp_main)
+    app.register_blueprint(bp_auth)
+    pass
